e47e0bb567d230c5aec6d41e808f7369575a554e
林輝泉 <arthur@linhuiquandeMacBook-Air.local>
2026-06-16 21:25:30 +0800

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD30cV4rdx1Ys4J4eGnznPieUUhOhWhHrQ",
+  authDomain: "teachstudy-b8b64.firebaseapp.com",
+  projectId: "teachstudy-b8b64",
+  storageBucket: "teachstudy-b8b64.firebasestorage.app",
+  messagingSenderId: "195283799004",
+  appId: "1:195283799004:web:b212e67d4e30ac95dddd19"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
